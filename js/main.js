ac3aa6b0e6ae0c58913205beb079c7a6944453cf
Sullivan Amadike <amadikesullivan20@gmail.com>
2022-04-28 11:17:30 +0100

    second codewar

--- js/main.js
+++ js/main.js
@@ -1,13 +1,38 @@
-function betterThanAverage(classPoints, yourPoints) {
-    let result = classPoints.reduce((acc,c) => acc + c)
-    let average = result / classPoints.length
+// function betterThanAverage(classPoints, yourPoints) {
+//     let result = classPoints.reduce((acc,c) => acc + c)
+//     let average = result / classPoints.length
     
-    if(yourPoints > average){
-      return true
+//     if(yourPoints > average){
+//       return true
+//     }else{
+//       return false
+//     }
+// }
+
+// let display = betterThanAverage([5,20,30], 10)
+// console.log(display)
+
+
+// const result = (classPoints, yourPoints) => classPoints.reduce((accumulate, index) => accumulate + index) / classPoints.length < yourPoints ? true : false
+
+// console.log(result([4,5,6,10],50))
+
+
+// second codewar
+let array = [5,6,6,5,5]
+function sum (numbers) {
+  
+    if(numbers.length === 0 && Array.isArray(numbers)){
+      return 0 
+  
     }else{
-      return false
-    }
+    
+    return numbers.reduce((acc, b) => acc + b)
+    
+  }
+
 }
 
-let display = betterThanAverage([5,20,30], 10)
-console.log(display)
+let result = sum ( array )
+console.log(result)
+    
