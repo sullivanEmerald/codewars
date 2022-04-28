52c4282b3751704ba319203979d351d307c9ca1f
Sullivan Amadike <amadikesullivan20@gmail.com>
2022-04-28 13:25:21 +0100

    third codewar

--- js/main.js
+++ js/main.js
@@ -19,20 +19,42 @@
 
 
 // second codewar
-let array = [5,6,6,5,5]
-function sum (numbers) {
+// let array = [5,6,6,5,5]
+// function sum (numbers) {
   
-    if(numbers.length === 0 && Array.isArray(numbers)){
-      return 0 
+//     if(numbers.length === 0 && Array.isArray(numbers)){
+//       return 0 
   
-    }else{
+//     }else{
     
-    return numbers.reduce((acc, b) => acc + b)
+//     return numbers.reduce((acc, b) => acc + b)
     
-  }
+//   }
 
+// }
+
+// let result = sum ( array )
+// console.log(result)
+// let input = [80, 85, 75, 55, 65, 60, 60, 80]
+
+// function calculateScore(arr){
+//     return result = arr.sort((a,b) => a-b)
+//     const obj = {
+
+//     }
+// }
+    
+// console.log(calculateScore(input))
+let array = [50, 70, 80, 90]
+
+// let result  = Math.min(...arr)
+// console.log(result)
+
+function smallestInteger(arr){
+    let sorterd = arr.sort((a,b) => a-b)
+    let max = sorterd[sorterd.length -1]
+    return max
 }
 
-let result = sum ( array )
-console.log(result)
-    
+let answer = smallestInteger(array)
+console.log(answer)
