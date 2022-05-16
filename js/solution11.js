9329b8f274b2c4957af623e44f20c9c8f52754a4
Sullivan Amadike <amadikesullivan20@gmail.com>
2022-05-16 23:21:41 +0100

    codewars submit 11

---
+++ js/solution11.js
@@ -0,0 +1,13 @@
+function getCount(str) {
+    var vowelsCount = 0;
+    str.split("").forEach(function(x){
+      if(x == "a" | x == "e" | x == "i" | x == "o" | x == "u"){
+        vowelsCount += 1;
+      }
+    });  
+    return vowelsCount;
+  }
+  
+  let answer = getCount('sullivanamadike')
+  console.log(answer)
+  
