950b8f7c624f7bd571f57f84589ea86ccc176833
Sullivan Amadike <amadikesullivan20@gmail.com>
2022-05-27 00:05:09 +0100

    codewar challenge17

--- js/codewar17.js
+++ js/codewar17.js
@@ -1,26 +1,14 @@
-// function between(a, b) {
-//     // your code here
-//     let array = []
-//     if(a < b){
-//       for(let i = a; i <= b; i++){
-//         array.push(i)
-//       }
-//     }
-//     return array
-//   }
-
-// let answer = between(5,20)
-// console.log(answer)
-
-function calculateYears(principal, interest, tax, desired) {
-    // your code
-  
-  let years = 0
-  
-  while(principal < desired){
-    principal += principal * interest * (1 - tax)
-    years++
+function between(a, b) {
+    // your code here
+    let array = []
+    if(a < b){
+      for(let i = a; i <= b; i++){
+        array.push(i)
+      }
+    }
+    return array
   }
-  
-    return years
-}
+
+let answer = between(5,20)
+console.log(answer)
+
